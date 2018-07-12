bfbd4b3a20d901726a9e4a3e6bd5c36bc0030d2b
zhangzhongjun <1014830422@qq.com>
2018-07-12 20:41:12 +0800

    2018.7.12更新

--- 智能合约_修改.js
+++ 智能合约_修改.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// n1uPusaT9nvynxKjPdoaacR4QCmBEZj9YaT
+// 构造函数
 var PasswordItem = function(text) {
     if (text) {
         var obj = JSON.parse(text);
@@ -11,20 +13,36 @@ var PasswordItem = function(text) {
     }
 };
 
-// owner表示拥有者
-// password表示密码
+PasswordItem.prototype = {
+    toString: function() {
+        return JSON.stringify(this);
+    }
+};
+
+
+// 构造函数
+// ownerPassword表示拥有着的密码
+// passwords表示该用户存储的密码
 var BoxItem = function(text) {
     if (text) {
         var obj = JSON.parse(text);
-        this.owner = obj.owner;
+		this.ownerPassword = obj.ownerPassword;
         this.passwords = obj.passwords;
     } else {
-        this.owner = "";
+		this.ownerPassword = "";
         this.passwords = new Array();
     }
 };
 
+BoxItem.prototype = {
+    toString: function() {
+        return JSON.stringify(this);
+    }
+};
+
+
 var PasswordBox = function() {
+	// 定义本地存储 key是用户的地址 value是boxItem
     LocalContractStorage.defineMapProperty(this, "boxs", {
         parse: function(text) {
             return new BoxItem(text);
@@ -40,6 +58,27 @@ PasswordBox.prototype = {
     init: function() {
 
     },
+	
+	// 注册函数
+	regist: function(ownerPassword){
+		
+		var boxItem = this.boxs.get(Blockchain.transaction.from);
+		if(boxItem){
+			throw new Error("you have regist befor,please now repeat regist!!")
+		}
+		
+		ownerPassword = ownerPassword.trim();
+		if (ownerPassword === ""){
+			throw new Error("empty owner password！！");
+		}
+		
+		var boxItem = new BoxItem();
+		boxItem.ownerPassword = ownerPassword;
+		
+		this.boxs.put(Blockchain.transaction.from,boxItem);
+		
+		return true;
+	},
 
 	// 保存函数
     save: function(cipherPassword, description) {
@@ -49,35 +88,34 @@ PasswordBox.prototype = {
             throw new Error("empty cipherPassword / description");
         }
 
-        var passwords;
+        
         var from = Blockchain.transaction.from;
         var boxItem = this.boxs.get(from);
 
-        if (boxItem) {
-            passwords = boxItem.passwords;
-        } else {
-            boxItem = new BoxItem();
-            passwords = new Array();
+        if (!boxItem) {
+			throw new Error("you haven't regist,please regist first!!!");
         }
-
+		
+		var passwords = boxItem.passwords;
+		
+		// 新建一个passwordItem对象
         var passwordItem = new PasswordItem();
         passwordItem.cipherPassword = cipherPassword;
         passwordItem.description = description;
-
+		// 将passwordItem对象插入到临时变量中
         passwords.push(passwordItem);
 
-
-        boxItem.owner = from;
         boxItem.passwords = passwords;
 
-        this.boxs.delete(from);
         this.boxs.put(from, boxItem);
+		
+		return true;
     },
 
+	// 获得所有存储的密码
     get: function() {
         var from = Blockchain.transaction.from;
-
-        return this.boxs.get(from);
+        return this.boxs.get(from).passwords;
     }
 };
 
